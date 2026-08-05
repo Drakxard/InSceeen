6ae6bdaca42301cbd9cae6539ce0b1b7a0c75f3d
Drakxard <drakxard@gmail.com>
2026-08-04 22:31:31 -0300

    Release modules in 1.7.0

--- mobile_android/app/build.gradle.kts
+++ mobile_android/app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.inscreen.mic"
         minSdk = 26
         targetSdk = 36
-        versionCode = 8
-        versionName = "1.6.1"
+        versionCode = 9
+        versionName = "1.7.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
