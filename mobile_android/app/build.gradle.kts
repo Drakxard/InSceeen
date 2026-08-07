91f30072121bc500a4833927720327809db93e9b
Drakxard <drakxard@gmail.com>
2026-08-07 15:25:20 -0300

    Release InScreen Mic v1.7.15

--- mobile_android/app/build.gradle.kts
+++ mobile_android/app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.inscreen.mic"
         minSdk = 26
         targetSdk = 36
-        versionCode = 23
-        versionName = "1.7.14"
+        versionCode = 24
+        versionName = "1.7.15"
 
         buildConfigField(
             "String",
