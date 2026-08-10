59e01ff8bffea549f5f75771e9cf69d34a600c58
Drakxard <drakxard@gmail.com>
2026-08-09 21:42:31 -0300

    Publica InScreen 2.0.1 con respaldo integral

--- mobile_android/app/build.gradle.kts
+++ mobile_android/app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.inscreen.mic"
         minSdk = 26
         targetSdk = 36
-        versionCode = 27
-        versionName = "2.0.0"
+        versionCode = 28
+        versionName = "2.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
