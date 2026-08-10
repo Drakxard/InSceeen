bc46f097ced4a87c69a6ed1c47a21da7f1df25e0
Drakxard <drakxard@gmail.com>
2026-08-10 09:35:01 -0300

    Publica InScreen 2.0.3 con apertura cómoda del detalle

--- mobile_android/app/build.gradle.kts
+++ mobile_android/app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.inscreen.mic"
         minSdk = 26
         targetSdk = 36
-        versionCode = 29
-        versionName = "2.0.2"
+        versionCode = 30
+        versionName = "2.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
